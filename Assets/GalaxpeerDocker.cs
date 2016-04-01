e97a74fb9efd31fbb82d60c2d61bff23cd5b1012
thijs <thijsboumans94@gmail.com>
2016-04-01 11:49:33 +0200

    try again

---
+++ Assets/GalaxpeerDocker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Galaxpeer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GalaxpeerDocker : MonoBehaviour {
+	public GameObject baseOtherPlayer;
+	public GameObject baseAsstroid;
+	public GameObject baseRocket; 
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		handleSpawns ();
+	}
+	
+	public void handleSpawns(){
+		UnityUnityInterface UUI = (UnityUnityInterface)UnityInterfaceInterfaceManager.InstanceUnintyInterface;
+		List<MobileEntity> toSpawn = UUI.getSpawns ();
+		foreach (MobileEntity spawn in toSpawn) {
+			switch(spawn.Type){
+				case MobileEntity.EntityType.Player:break;
+				case MobileEntity.EntityType.Rocket:break;
+				case MobileEntity.EntityType.Asteroid:break;
+			}
+		}
+	}
+}
---
+++ Assets/MobileEntityGameObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Galaxpeer;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MobileEntityGameObject : MonoBehaviour {
+	public Guid Uuid;
+	
+	// Update is called once per frame
+	void Update () {
+		UnityUnityInterface UUI = (UnityUnityInterface)UnityInterfaceInterfaceManager.InstanceUnintyInterface;
+		MobileEntity me = UUI.GetEntity (Uuid);
+		Transform t=transform;
+		var pos = me.Location;
+		t.position = new UnityEngine.Vector3(pos.X,pos.Y,pos.Z);
+		var rot = me.Rotation;
+		t.rotation = new Quaternion (rot.X, rot.Y, rot.Z, rot.W);
+		var velo = me.Velocity;
+	}
+}
