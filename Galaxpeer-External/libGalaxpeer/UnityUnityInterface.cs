dd2118b76ee523aec42acfdca11d069979bba6e9
thijs <thijsboumans94@gmail.com>
2016-04-01 09:16:48 +0200

    unity interfaces

---
+++ Galaxpeer-External/libGalaxpeer/UnityUnityInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Galaxpeer
+{
+	public class UnityUnityInterface : UnityInterfaceInterface
+	{
+		public UnityUnityInterface()
+		{
+			
+			public override void SpawnModel(MobileEntity a);
+			public override void Destroy(MobileEntity a);
+		}
+	}
+}
+
